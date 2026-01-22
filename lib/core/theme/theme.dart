dd1eae6af7a2ef177f64f1ed334ddf55fd03b446
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-01-22 16:37:16 +0500

    add(AppTheme): this has been cleaned a bit

--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -2,15 +2,15 @@ import 'package:client/core/theme/app_pallete.dart';
 import 'package:flutter/material.dart';
 
 class AppTheme {
+  static _border(Color borderColor) => OutlineInputBorder(
+    borderSide: BorderSide(color: borderColor, width: 3),
+    borderRadius: BorderRadius.all(Radius.circular(10.0)),
+  );
   static final darkThemeMode = ThemeData.dark().copyWith(
     scaffoldBackgroundColor: Pallete.backgroundColor,
     inputDecorationTheme: InputDecorationTheme(
-      focusedBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Pallete.gradient1, width: 3),
-      ),
-      enabledBorder: OutlineInputBorder(
-        borderSide: BorderSide(color: Pallete.borderColor, width: 3),
-      ),
+      focusedBorder: _border(Pallete.gradient2),
+      enabledBorder: _border(Pallete.borderColor),
       contentPadding: EdgeInsets.all(27),
     ),
   );
--- lib/features/auth/view/pages/signup_page.dart
+++ lib/features/auth/view/pages/signup_page.dart
@@ -13,16 +13,28 @@ class _SignUpPageState extends State<SignUpPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Column(
-        children: [
-          const Text(
-            "Sign up",
-            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 44),
-          ),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              "Sign up",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 44),
+            ),
+            const SizedBox(height: 30),
 
-          // FIELDS!
-          CustomField(hintText: "Name"),
-        ],
+            // FIELDS!
+            CustomField(hintText: "Name"),
+            const SizedBox(height: 20),
+
+            CustomField(hintText: "Email"),
+            const SizedBox(height: 20),
+
+            CustomField(hintText: "Password"),
+            const SizedBox(height: 20),
+          ],
+        ),
       ),
     );
   }
