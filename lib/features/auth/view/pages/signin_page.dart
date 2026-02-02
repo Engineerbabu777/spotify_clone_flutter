c10cf2ac129c7f38b80a909710987ea4dd69666f
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-02-02 23:51:08 +0500

    add(SigninPage): this is now fully ready

--- lib/features/auth/view/pages/signin_page.dart
+++ lib/features/auth/view/pages/signin_page.dart
@@ -2,9 +2,12 @@ import 'package:client/core/theme/app_pallete.dart';
 import 'package:client/features/auth/view/pages/signup_page.dart';
 import 'package:client/features/auth/view/widgets/auth_gradient_button.dart';
 import 'package:client/features/auth/view/widgets/custom_field.dart';
+import 'package:client/features/auth/view/widgets/loader.dart';
+import 'package:client/features/auth/viewmodel/auth_viewmodel.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class SigninPage extends StatefulWidget {
+class SigninPage extends ConsumerStatefulWidget {
   static MaterialPageRoute route() => MaterialPageRoute(
     builder: (context) {
       return SigninPage();
@@ -14,10 +17,10 @@ class SigninPage extends StatefulWidget {
   const SigninPage({super.key});
 
   @override
-  State<SigninPage> createState() => _SigninPageState();
+  ConsumerState<SigninPage> createState() => _SigninPageState();
 }
 
-class _SigninPageState extends State<SigninPage> {
+class _SigninPageState extends ConsumerState<SigninPage> {
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
 
@@ -32,66 +35,92 @@ class _SigninPageState extends State<SigninPage> {
 
   @override
   Widget build(BuildContext context) {
+    final isLoading = ref.watch(authViewModelProvider)?.isLoading == true;
+
+    ref.listen(authViewModelProvider, (prev, next) {
+      next?.when(
+        data: (data) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(content: Text('Login Successfully!')));
+
+          // Navigator.push(context, HomePage);
+        },
+        error: (error, st) {
+          ScaffoldMessenger.of(context)
+            ..hideCurrentSnackBar()
+            ..showSnackBar(SnackBar(content: Text(error.toString())));
+        },
+        loading: () {},
+      );
+    });
+
     return Scaffold(
       appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
-        child: Form(
-          key: formKey,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              const Text(
-                "Sign up",
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 44),
-              ),
-              const SizedBox(height: 30),
+      body: isLoading
+          ? CustomLoader()
+          : Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Form(
+                key: formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    const Text(
+                      "Sign up",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 44,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
 
-              // FIELDS!
-              CustomField(hintText: "Email", controller: emailController),
-              const SizedBox(height: 20),
+                    // FIELDS!
+                    CustomField(hintText: "Email", controller: emailController),
+                    const SizedBox(height: 20),
 
-              CustomField(
-                hintText: "Password",
-                controller: passwordController,
-                isObscure: true,
-              ),
-              const SizedBox(height: 20),
+                    CustomField(
+                      hintText: "Password",
+                      controller: passwordController,
+                      isObscure: true,
+                    ),
+                    const SizedBox(height: 20),
 
-              // BUTTONS!
-              AuthGradientButton(
-                fnHandler: () async {
-                  if (!formKey.currentState!.validate()) return;
-                },
-                text: "Sign In",
-              ),
+                    // BUTTONS!
+                    AuthGradientButton(
+                      fnHandler: () async {
+                        if (!formKey.currentState!.validate()) return;
+                        ref.read(authViewModelProvider.notifier).signInUser(email: emailController.text, password: passwordController.text);
+                      },
+                      text: "Sign In",
+                    ),
 
-              const SizedBox(height: 20),
-              GestureDetector(
-                onTap: () {
-                  Navigator.push(context, SignUpPage.route());
-                },
-                child: RichText(
-                  text: TextSpan(
-                    style: Theme.of(context).textTheme.titleMedium,
+                    const SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.push(context, SignUpPage.route());
+                      },
+                      child: RichText(
+                        text: TextSpan(
+                          style: Theme.of(context).textTheme.titleMedium,
 
-                    children: [
-                      TextSpan(text: "Don't have an account? "),
-                      TextSpan(
-                        text: "Sign Up",
-                        style: TextStyle(
-                          color: Pallete.gradient2,
-                          fontWeight: FontWeight.bold,
+                          children: [
+                            TextSpan(text: "Don't have an account? "),
+                            TextSpan(
+                              text: "Sign Up",
+                              style: TextStyle(
+                                color: Pallete.gradient2,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
                         ),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
