b25689961af0a0f631340237cd24f3abd2fc734f
EngineerBabu777 <awaismumtaz0099@gmail.com>
2026-02-04 00:31:23 +0500

    add(AuthLocalRepository): is now ready to use

--- lib/features/auth/repositories/auth_local_repository.dart
+++ lib/features/auth/repositories/auth_local_repository.dart
@@ -1,6 +1,22 @@
-
-
+import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthLocalRepository {
-  
+  late SharedPreferences _sharedPreferences;
+
+  Future<void> init() async {
+    _sharedPreferences = await SharedPreferences.getInstance();
+  }
+
+  void setToken(String? token) {
+    if (token != null) {
+      _sharedPreferences.setString('X-auth-token', token);
+    }
+  }
+
+  String? getToken(String? token) {
+    if (token != null) {
+      return _sharedPreferences.getString('X-auth-token');
+    }
+    return null;
+  }
 }
